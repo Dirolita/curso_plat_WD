cf4dfabcdbe32fb8478f0fe745efd0cb621f4e40
Diana Carolina Serrano <dianacaroserranot@gmail.com>
2023-12-14 15:24:26 -0500

    Funcionalidad del menu en resposive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector('.email-nav');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+//Menu hamburguesa 
+const menuMobile = document.querySelector('.menu');
+const mobileMenuInfo = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click',toggledesktopMenu);
+menuMobile.addEventListener('click', toggleMenuMobile)
 
 function toggledesktopMenu(){
-    /* Quitar la clase que desaparece el menu*/
+    /* Quitar y poner la clase que desaparece el menu*/
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMenuMobile(){
+    mobileMenuInfo.classList.toggle('inactive');
 }
