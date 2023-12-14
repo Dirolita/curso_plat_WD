36db5c5827eb5e023f932ad402a7c81ef854b4c7
Diana Carolina Serrano <dianacaroserranot@gmail.com>
2023-12-14 17:14:38 -0500

    Funcion del carrito de compras

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-const menuEmail = document.querySelector('.email-nav');
+//Menu Desktop
+const menuDesktop = document.querySelector('.email-nav');
 const desktopMenu = document.querySelector('.desktop-menu');
-
 //Menu hamburguesa 
 const menuMobile = document.querySelector('.menu');
 const mobileMenuInfo = document.querySelector('.mobile-menu');
+//navbar-shopping-cart
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click',toggledesktopMenu);
-menuMobile.addEventListener('click', toggleMenuMobile)
+//Liseners
+menuDesktop.addEventListener('click',toggledesktopMenu);
+menuMobile.addEventListener('click', toggleMenuMobile);
+shoppingCart.addEventListener('click',toggleProductDetail);
 
+//Functions
 function toggledesktopMenu(){
-    /* Quitar y poner la clase que desaparece el menu*/
+    productDetail.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
-
 function toggleMenuMobile(){
+    productDetail.classList.add('inactive');
     mobileMenuInfo.classList.toggle('inactive');
+}
+function toggleProductDetail(){
+    mobileMenuInfo.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetail.classList.toggle('inactive');
 }
